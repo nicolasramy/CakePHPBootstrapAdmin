e1b2817d8b5eeb9b5096cf18636bbcb34499dc4e
Nicolas RAMY-SEPOU <nicolas.ramy@gmail.com>
2012-06-07 23:27:18 +0200

    Update BootstrapAdminAppController default components & helpers

--- Controller/BootstrapAdminAppController.php
+++ Controller/BootstrapAdminAppController.php
@@ -2,7 +2,11 @@
 
 class BootstrapAdminAppController extends AppController {
 
+	//public $components = array('Auth');
+	public $helpers = array('BootstrapAdmin.Navbar');
 
-
+	public function beforeFilter() {
+		//var_dump($this); exit;
+	}
 
 }
